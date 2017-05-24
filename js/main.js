39a2bccc331a36d6cb8aeb165e0b1b7b391303ea
Matt Tyas <matthewtyas@hotmail.com>
2017-05-24 17:31:32 +0100

    typography

---
+++ js/main.js
@@ -0,0 +1,40 @@
+  // init controller
+	// var controller = new ScrollMagic.Controller();
+	//
+	// new ScrollMagic.Scene({
+	// 	triggerElement: "#interaction",
+	// 	triggerHook: 'onLeave',
+	// 	duration: 350
+	// })
+	// .setClassToggle(".body", "is--interaction")
+	// .addIndicators()
+	// .addTo(controller);
+	//
+	// new ScrollMagic.Scene({
+	// 	triggerElement: "#product",
+	// 	triggerHook: 'onLeave',
+	// 	duration: 250
+	// })
+	// .setClassToggle(".body", "is--product")
+	// .addIndicators()
+	// .addTo(controller);
+
+
+	// new ScrollMagic.Scene({
+	// 	triggerElement: "#service",
+	// 	duration: 200,
+	// 	offset: 200
+	// })
+	// .setClassToggle("#service", "is--active")
+	// .removeClassToggle(true);
+	// .addIndicators()
+	// .addTo(controller);
+  //
+	// new ScrollMagic.Scene({
+	// 	triggerElement: "#service",
+	// 	duration: 200,
+	// 	offset: 200
+	// })
+	// .setClassToggle(".body", "is--service")
+	// .removeClassToggle(true);
+	// .addTo(controller);
